0b65665222627af29eb355404e5d8aa3fdf96108
William Wyatt <wwyatt@mapanything.com>
2018-10-11 13:07:44 -0400

    basic setup to test

--- android/src/main/java/com/reactlibrary/RNToolboxModule.java
+++ android/src/main/java/com/reactlibrary/RNToolboxModule.java
@@ -1,6 +1,9 @@
 
 package com.reactlibrary;
 
+import android.hardware.fingerprint.FingerprintManager
+import android.os.Build
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -18,5 +21,25 @@ public class RNToolboxModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNToolbox";
+  }
+
+  @ReactMethod
+  public void isAvailable(final Promise promise) {
+    try {
+      FingerprintManager manager = getFingerprintManager();
+      boolean v = (manager != null && manager.isHardwareDetected() && manager.hasEnrolledFingerprints());
+      promise.resolve(v);
+    } catch (Exception ex) {
+      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+    }
+  }
+
+  @ReactMethod
+  private FingerprintManager getFingerprintManager() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      return (FingerprintManager) reactContext.getSystemService(reactContext.FINGERPRINT_SERVICE);
+    } else {
+      return null;
+    }
   }
 }
--- ios/RNToolbox/RNToolbox.m
+++ ios/RNToolbox/RNToolbox.m
@@ -1,5 +1,6 @@
 
 #import "RNToolbox.h"
+#import <LocalAuthentication/LocalAuthentication.h>
 
 @implementation RNToolbox
 
@@ -8,6 +9,15 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(isAvailable: (RCTPromiseResolveBlock)resolve rejector:(RCTPromiseRejectBlock)reject) {
+    LAContext *context = [[LAContext alloc] init];
+    if ([context canEvaluatePolicy:kLAPolicyDeviceOwnerAuthenticationWithBiometrics error: NULL]) {
+        resolve(@(YES));
+    } else {
+        resolve(@(NO));
+    }
+}
 
 @end
   
