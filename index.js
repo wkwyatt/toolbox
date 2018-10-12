e763f3048d1f6cb812e221280f3e20d23a470cad
William Wyatt <wwyatt@mapanything.com>
2018-10-12 18:18:41 -0400

    test Util and batterymanager

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import Util from './src/Util';
 
 const { RNToolbox, BatteryManager } = NativeModules;
 
-const TBBatteryManager = new NativeEventEmitter(BatteryManager);
+// const TBBatteryManager = new NativeEventEmitter(BatteryManager);
 const TB = Util.native('RNToolbox');
 const TBBattery = Util.native('BatteryManager');
 
-export default { RNToolbox, TBBatteryManager, TB, TBBattery };
+export default { RNToolbox, BatteryManager, TBBattery };
